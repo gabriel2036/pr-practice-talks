98bca1fe89eaf8e139c2970dcc81c39c3857bd51
Gabriel <herreduarteb@gmail.com>
2022-10-21 10:21:10 -0300

    Teste

---
+++ soma.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int somador(int a, int b){
+	int soma;
+	soma = a+b;
+	return soma;
+	
+}
+
+
+int main(){
+
+
+	int a = 10;
+	int b = 20;
+	
+	
+	printf("%d",somador(a,b));
+}
